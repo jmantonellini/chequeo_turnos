3de3891ca1217ea9fbf0e22a4bdbfbeb1a77b3fb
Juan Manuel Antonellini <juanmantonellini@gmail.com>
2024-11-20 08:45:01 -0500

    Added screenshot and private chat

--- chequeo.py
+++ chequeo.py
@@ -9,17 +9,35 @@ from playwright.async_api import async_playwright
 warnings.filterwarnings("ignore", category=UserWarning, module="urllib3")
 
 TELEGRAM_CHANNEL_ID = "-1002337504567"
+TELEGRAM_CHAT_ID = "1579035548"
 TELEGRAM_TOKEN = "7393396454:AAHO7wrC-cvZXksPVJxyPMXMrApUAdYe8sw"
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
-async def send_telegram_message(message="Hello, World!"):
+async def send_channel_message(message="Hello, World!"):
     try:
         await bot.send_message(chat_id=TELEGRAM_CHANNEL_ID, text=message)
         print("Mensaje enviado con éxito.")
     except Exception as e:
         print("Error al enviar el mensaje de Telegram:", e)
+
+
+async def send_chat_message(message="Hello, World!"):
+    try:
+        await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        print("Mensaje enviado con éxito.")
+    except Exception as e:
+        print("Error al enviar el mensaje de Telegram:", e)
+
+
+async def send_telegram_photo(photo_path):
+    try:
+        with open(photo_path, "rb") as photo:
+            await bot.send_photo(chat_id=TELEGRAM_CHANNEL_ID, photo=photo)
+        print("Foto enviada con éxito.")
+    except Exception as e:
+        print("Error al enviar la foto de Telegram:", e)
 
 
 async def automate_workflow():
@@ -33,14 +51,14 @@ async def automate_workflow():
         # Launch the browser
         browser = await p.chromium.launch(
             headless=True
-        )  # Use headless=True for headless mode
+        )
         context = await browser.new_context()
         page = await context.new_page()
 
         # Step 1: Navigate to the page
         await page.goto(
             "https://www.exteriores.gob.es/Consulados/cordoba/es/Comunicacion/Noticias/Paginas/Articulos/Instrucciones-para-solicitar-cita-previa-para-LMD.aspx"
-        )  # Replace with the actual page URL
+        )
 
         await page.wait_for_timeout(3000)
 
@@ -62,29 +80,36 @@ async def automate_workflow():
         page.on("dialog", lambda dialog: dialog.accept())  # Automatically click "OK"
 
         # Step 4: Wait for a few seconds after clicking the dialog
+        print("Esperando 3 segundos...")
         await page.wait_for_timeout(
             3000
         )  # Wait for 3 seconds to ensure the next page loads
 
         # Step 5: Click the green "Continuar / Continue" button
         continuar_locator = page.locator("button", has_text="Continuar")
+        print("Clickeando el botón 'Continuar'...")
         await continuar_locator.click()
 
         # Step 6: Wait for a few seconds to allow the API call to complete
+        print("Esperando 30 segundos...")
         await page.wait_for_timeout(30000)
 
         # Step 7: Print the AllowAppointments property
+        current_time = time.strftime("%H:%M:%S", time.localtime())
+        print(f"Chequeo hecho a las: ({current_time})")
         mensaje_locator = page.locator(
             "div#idDivBktServicesContainer", has_text="No hay horas"
         )
-        current_time = time.strftime("%H:%M:%S", time.localtime())
         if await mensaje_locator.is_visible():
             print("No hay horas disponibles.")
-            await send_telegram_message(
+            await send_chat_message(
                 f"No hay horas, intentaré luego. Chequeo hecho a las: ({current_time})"
             )
         else:
-            await send_telegram_message(
+            screenshot_path = "screenshot.png"
+            await page.screenshot(path=screenshot_path)
+            await send_telegram_photo(screenshot_path)
+            await send_channel_message(
                 "FIJATE AHORA!! HAY TURNOS DISPONIBLES!! Chequeo hecho a las: ({current_time})"
             )
 
